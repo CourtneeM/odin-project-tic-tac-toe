6999b3625d5f09000cd74a307eb327ec87b9fb38
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-06-19 11:45:14 -0500

    Add alternating symbols and disable board after game won

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const gameBoard = (() => {
-  const div = document.createElement("div");
-  let gameboard = () => ["x", "x", "o", 
-                         "x", "o", "o", 
-                         "o", "", "o"];
-  return {gameboard};
+  let gameboardArr = () => ["", "", "", 
+                            "", "", "", 
+                            "", "", ""];
+  return {gameboardArr};
 })();
 
 const Player = (name, symbol) => {
@@ -13,54 +12,77 @@ const Player = (name, symbol) => {
 }
 
 const gamePlay = (() => {
-  let win = () => {
-    let gameboard = gameBoard.gameboard();
-    if((gameboard[0] === "x" && gameboard[1] === "x" && gameboard[2] === "x") 
-    || (gameboard[3] === "x" && gameboard[4] === "x" && gameboard[5] === "x")
-    || (gameboard[6] === "x" && gameboard[7] === "x" && gameboard[8] === "x")
-    || (gameboard[0] === "x" && gameboard[3] === "x" && gameboard[6] === "x")
-    || (gameboard[1] === "x" && gameboard[4] === "x" && gameboard[7] === "x")
-    || (gameboard[2] === "x" && gameboard[5] === "x" && gameboard[8] === "x")
-    || (gameboard[0] === "x" && gameboard[4] === "x" && gameboard[8] === "x")
-    || (gameboard[2] === "x" && gameboard[4] === "x" && gameboard[6] === "x"))
-    { return `${player1.getName()} wins!`;
-    } else if((gameboard[0] === "o" && gameboard[1] === "o" && gameboard[2] === "o") 
-           || (gameboard[3] === "o" && gameboard[4] === "o" && gameboard[5] === "o")
-           || (gameboard[6] === "o" && gameboard[7] === "o" && gameboard[8] === "o")
-           || (gameboard[0] === "o" && gameboard[3] === "o" && gameboard[6] === "o")
-           || (gameboard[1] === "o" && gameboard[4] === "o" && gameboard[7] === "o")
-           || (gameboard[2] === "o" && gameboard[5] === "o" && gameboard[8] === "o")
-           || (gameboard[0] === "o" && gameboard[4] === "o" && gameboard[8] === "o")
-           || (gameboard[2] === "o" && gameboard[4] === "o" && gameboard[6] === "o"))
-           { return `${player2.getName()} wins!`; 
-    } else {
-      return `nobody wins`;
+  let gameboardArr = gameBoard.gameboardArr();
+  let gameWon = false;
+  let moveCount = 0;
+  const gameboard = document.getElementById("gameboard");
+  gameboard.addEventListener('click', e => {
+    if(gameWon) {
+      return;
     }
-    // Look at refactoring win conditions later ( look at string.match() )
-    // let gameboardTop = gameboard.slice(0, 3);
-    // let horizontalWin = `x,x,x`;
-    // if(String(gameboardTop) === horizontalWin) {
+    playerMove(e);
+    win();
+  });
+  
+  let playerMove = e => {
+    if(e.target.classList.contains("gameboard-square")) {
+      if(e.target.textContent === "X" || e.target.textContent === "O") {
+        return;
+      }
+      if(moveCount % 2 === 0) {
+        gameboardArr[e.target.id] = player1.getSymbol();
+        e.target.textContent = player1.getSymbol();
+      } else if(moveCount % 2 === 1) {
+        gameboardArr[e.target.id] = player2.getSymbol();
+        e.target.textContent = player2.getSymbol();
+      }
+      moveCount++;
+    }
+    console.log(gameboardArr);
+  };
+  const win = () => {
+    if((gameboardArr[0] === "X" && gameboardArr[1] === "X" && gameboardArr[2] === "X") 
+    || (gameboardArr[3] === "X" && gameboardArr[4] === "X" && gameboardArr[5] === "X")
+    || (gameboardArr[6] === "X" && gameboardArr[7] === "X" && gameboardArr[8] === "X")
+    || (gameboardArr[0] === "X" && gameboardArr[3] === "X" && gameboardArr[6] === "X")
+    || (gameboardArr[1] === "X" && gameboardArr[4] === "X" && gameboardArr[7] === "X")
+    || (gameboardArr[2] === "X" && gameboardArr[5] === "X" && gameboardArr[8] === "X")
+    || (gameboardArr[0] === "X" && gameboardArr[4] === "X" && gameboardArr[8] === "X")
+    || (gameboardArr[2] === "X" && gameboardArr[4] === "X" && gameboardArr[6] === "X"))
+    { gameWon = true;
+      console.log( `${player1.getName()} wins!` );
+    } else if((gameboardArr[0] === "O" && gameboardArr[1] === "O" && gameboardArr[2] === "O") 
+    || (gameboardArr[3] === "O" && gameboardArr[4] === "O" && gameboardArr[5] === "O")
+    || (gameboardArr[6] === "O" && gameboardArr[7] === "O" && gameboardArr[8] === "O")
+    || (gameboardArr[0] === "O" && gameboardArr[3] === "O" && gameboardArr[6] === "O")
+    || (gameboardArr[1] === "O" && gameboardArr[4] === "O" && gameboardArr[7] === "O")
+    || (gameboardArr[2] === "O" && gameboardArr[5] === "O" && gameboardArr[8] === "O")
+    || (gameboardArr[0] === "O" && gameboardArr[4] === "O" && gameboardArr[8] === "O")
+    || (gameboardArr[2] === "O" && gameboardArr[4] === "O" && gameboardArr[6] === "O"))
+    { gameWon = true;
+      return `${player2.getName()} wins!`; 
+  } else if(moveCount === 9) {
+    console.log( `nobody wins` );
+  }
+  // Look at refactoring win conditions later ( look at string.match() )
+  // let gameboardTop = gameboardArr.slice(0, 3);
+  // let horizontalWin = `x,x,x`;
+  // if(String(gameboardTop) === horizontalWin) {
     //   return `${player1.getName()} wins!`;
     // }
-  }
-  let checkForWin = () => {
-
-  }
-  return {win};
+  };
 })();
 
-let render = () => {
-  let gameboard = gameBoard.gameboard();
-  let gameboardSquares = document.querySelectorAll("#gameboard div");
-  for(let i = 0; i < gameboardSquares.length; i++) {
-    gameboardSquares[i].textContent = gameboard[i];
-  }
-}
-render();
-let player1 = Player("c", "X");
-let player2 = Player("t", "O");
+// let render = () => {
+//   let gameboardArr = gameBoard.gameboardArr();
+//   let gameboardSquares = document.querySelectorAll("#gameboard div");
+//   for(let i = 0; i < gameboardSquares.length; i++) {
+//     gameboardSquares[i].textContent = gameboardArr[i];
+//   }
+// }
+
+// render();
+let player1 = Player("coco", "X");
+let player2 = Player("tee", "O");
 console.log(player1.getName(), player1.getSymbol());
 console.log(player2.getName(), player2.getSymbol());
-console.log(gameBoard.gameboard());
-
-console.log(gamePlay.win());
